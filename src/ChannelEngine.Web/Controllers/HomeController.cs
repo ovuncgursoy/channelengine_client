5c4564c2b5533690218c43c0be3fbc23dbb365f4
Ovunc Gursoy <gursoy_ovunc@outlook.com>
2022-01-14 19:17:58 +0300

    Implement business logic

--- src/ChannelEngine.Web/Controllers/HomeController.cs
+++ src/ChannelEngine.Web/Controllers/HomeController.cs
@@ -1,17 +1,38 @@
 ﻿namespace ChannelEngine.Web.Controllers
 {
+    using ChannelEngine.Shared.Client;
     using ChannelEngine.Web.Models;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Logging;
     using System.Diagnostics;
+    using System.Linq;
+    using System.Threading.Tasks;
 
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IChannelEngineClient channelEngineClient;
 
-        public HomeController(ILogger<HomeController> logger) => _logger = logger;
+        public HomeController(IChannelEngineClient channelEngineClient, ILogger<HomeController> logger) : base(logger) 
+            => this.channelEngineClient = channelEngineClient;
 
-        public IActionResult Index() => View();
+        public async Task<IActionResult> Index()
+        {
+            var orders = await channelEngineClient.FetchInProgressOrders().ConfigureAwait(false);
+
+            return View(new OrdersViewModel()
+            {
+                Orders = orders.ToList(),
+                Products = orders.TopNProductsSold(-5).ToList()
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(string product, int quantity)
+        {
+            var result = await channelEngineClient.UpdateProductQuantity(product, quantity).ConfigureAwait(false);
+
+            return new JsonResult(new { Message = $"Stock for '{product}' has been updated to {quantity}. ({result.Message.ToLowerInvariant() })" });
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() =>
---
+++ src/ChannelEngine.Web/Models/OrdersViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ChannelEngine.Web.Models
+{
+    using ChannelEngine.Shared.Client.Types;
+    using System.Collections.Generic;
+
+    public class OrdersViewModel
+    {
+        public List<Order> Orders { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
