8482d7983dc9e817ec2882c57c2a076c2c5bce69
Ovunc Gursoy <gursoy_ovunc@outlook.com>
2022-01-14 19:17:50 +0300

    Add basic exception logging

---
+++ src/ChannelEngine.Web/Controllers/BaseController.cs
@@ -0,0 +1,23 @@
+﻿namespace ChannelEngine.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+    using Microsoft.Extensions.Logging;
+
+    public class BaseController : Controller
+    {
+        private readonly ILogger logger;
+
+        public BaseController(ILogger logger) => this.logger = logger;
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (context.Exception != null)
+            {
+                logger.LogError(context.Exception, "An error occured while handling your request");
+            }
+
+            base.OnActionExecuted(context);
+        }
+    }
+}
